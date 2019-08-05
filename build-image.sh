68d59efb0b9a2c42e0bf86ee9590f1d9272871cc
Toshiaki Maki <tmaki@pivotal.io>
2019-08-06 02:12:14 +0900

    Update script

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,6 @@ set -e
 
 VERSION=$(grep '<version>' pom.xml | head -n 2 | tail -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's| ||g')
 
-pack build making/blog-frontend:${VERSION} -p blog-frontend-server/target --builder cloudfoundry/cnb:bionic --publish
+mkdir blog-frontend-server/target/jar
+cp blog-frontend-server/target/*.jar blog-frontend-server/target/jar
+pack build making/blog-frontend:${VERSION} -p blog-frontend-server/target/jar --builder cloudfoundry/cnb:bionic --publish
