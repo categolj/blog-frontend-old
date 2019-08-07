2d883b23ca7f3d1a6d71302cfa07fcb9fe5588da
Toshiaki Maki <tmaki@pivotal.io>
2019-08-08 02:53:36 +0900

    Use default builder fixes #5

--- build-image.sh
+++ build-image.sh
@@ -5,4 +5,4 @@ VERSION=$(grep '<version>' pom.xml | head -n 2 | tail -n 1 | sed -e 's|<version>
 
 mkdir blog-frontend-server/target/jar
 cp blog-frontend-server/target/*.jar blog-frontend-server/target/jar
-pack build making/blog-frontend:${VERSION} -p blog-frontend-server/target/jar --builder cloudfoundry/cnb:bionic --publish
+pack build making/blog-frontend:${VERSION} -p blog-frontend-server/target/jar --publish
