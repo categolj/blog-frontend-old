f0679a8d63ae7586d1e0cdedf3ab13dc89a0a1dc
Toshiaki Maki <tmaki@pivotal.io>
2019-08-14 02:35:11 +0900

    Polish

--- blog-frontend-ui/src/components/NoMatch.js
+++ blog-frontend-ui/src/components/NoMatch.js
@@ -1,14 +1,15 @@
 import React from "react";
+import {Panel} from 'pivotal-ui/react/panels';
 
 export class NoMatch extends React.Component {
     render() {
         return (
-            <div>
+            <Panel>
                 <h2>404 Not Found</h2>
                 <p>
                     ¯\_(ツ)_/¯
                 </p>
-            </div>
+            </Panel>
         );
     }
 }
