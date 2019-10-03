192a2e180fb15f2cf3dc63eded1985670ec786ef
Toshiaki Maki <tmaki@pivotal.io>
2019-10-04 00:33:19 +0900

    Bump to Spring Boot 2.2.0.RC1

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
+set -ex
 
 export REACT_APP_BLOG_API=https://blog-api.ik.am
 export REACT_APP_BLOG_UI=https://blog.ik.am
 BOOT_VERSION=$(grep '<version>' pom.xml | head -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's|<.*>||g' -e 's| ||g')
 ./mvnw clean package -Dspring-boot.version=${BOOT_VERSION} -DskipTests=true  && ./build-image.sh
 
-kbld -f k8s | kubectl apply -f -
+#kbld -f k8s | kubectl apply -f -
+./
+deploy.sh
