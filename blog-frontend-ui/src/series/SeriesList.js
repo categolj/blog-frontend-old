80502f3b5c26538c6625ad83b71545b33ca8bd23
Toshiaki Maki <tmaki@pivotal.io>
2019-12-15 14:27:03 +0900

    Use external API

--- blog-frontend-ui/src/series/SeriesList.js
+++ blog-frontend-ui/src/series/SeriesList.js
@@ -2,15 +2,20 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export class SeriesList extends React.Component {
-    static content = [
-        {name: 'Cloud Foundryハンズオン資料', tag: 'Cloud Foundry Handson'},
-        {name: 'Concourseハンズオン資料', tag: 'Concourse Handson'},
-        {name: 'Kubernetesハンズオン資料', tag: 'Kubernetes Handson'},
-        {name: 'Spring WebFlux.fnハンズオン資料', tag: 'Spring WebFlux.fn Handson'},
-    ];
+    state = {
+        content: []
+    };
+
+    async componentDidMount() {
+        const content = await fetch('https://raw.githubusercontent.com/categolj/misc/master/series.json')
+            .then(data => data.json());
+        this.setState({
+            content: content
+        });
+    }
 
     render() {
-        const list = SeriesList.content.map(s =>
+        const list = this.state.content.map(s =>
             <li key={s.name}><Link to={`/series/${s.tag}/entries`}>{s.name}</Link></li>);
         return (<div>
             <h3 id="series" className={"home"}>Series</h3>
