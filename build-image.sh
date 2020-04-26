89e50e2feb877d7f058f3051d3d63ef23e357973
Toshiaki Maki <tmaki@pivotal.io>
2020-04-26 20:20:38 +0900

    Remove unused files

--- build-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-VERSION=$(grep '<version>' pom.xml | head -n 2 | tail -n 1 | sed -e 's|<version>||g' -e 's|</version>||g' -e 's| ||g')
-
-pack build making/blog-frontend:${VERSION} \
-  -p  blog-frontend-server/target/blog-frontend-server-*.jar \
-  --builder making/java-cnb-builder \
-  --publish
