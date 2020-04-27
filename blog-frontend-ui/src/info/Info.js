4b1aa29ac64c03d7e9dad64e92f4133e9df3cf67
Toshiaki Maki <tmaki@pivotal.io>
2020-04-27 19:20:17 +0900

    Add Like info

--- blog-frontend-ui/src/info/Info.js
+++ blog-frontend-ui/src/info/Info.js
@@ -10,6 +10,7 @@ export function Info() {
             <h2>Build Info</h2>
             <Block header={"UI"} url={ui}/>
             <Block header={"API"} url={api}/>
+            <Block header={"LIKE"} url={'https://like.dev.ik.am/actuator/info'}/>
             <Block header={"GATEWAY"} url={'https://blog.ik.am/management/info'}/>
         </Panel>
     );
