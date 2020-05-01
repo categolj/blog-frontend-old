0a6ae1a79e2c293dac07b58cab70ffe0746cee07
Toshiaki Maki <tmaki@pivotal.io>
2020-05-01 15:02:32 +0900

    Revert "Trace fetch call"
    This reverts commit 6d4cfb7674649f89b62100356db68ab70772c5a6.

--- blog-frontend-ui/src/entries/LikeService.js
+++ blog-frontend-ui/src/entries/LikeService.js
@@ -1,17 +1,11 @@
-import zipkinFetch from "../ZipkinFetch";
-
 class LikeService {
-    constructor() {
-        this.fetch = zipkinFetch.wrap('like');
-    }
-
     loadLikes(entryId) {
-        return this.fetch(`${process.env.REACT_APP_LIKE_API}/likes/${entryId}`)
+        return fetch(`${process.env.REACT_APP_LIKE_API}/likes/${entryId}`)
             .then(res => res.json());
     }
 
     postLikes(entryId) {
-        return this.fetch(`${process.env.REACT_APP_LIKE_API}/likes/${entryId}`, {method: 'POST'})
+        return fetch(`${process.env.REACT_APP_LIKE_API}/likes/${entryId}`, {method: 'POST'})
             .then(res => res.json());
     }
 }
