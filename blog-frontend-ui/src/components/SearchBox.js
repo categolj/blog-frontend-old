6be98c708d444bca5600cefd8af410f94d1952ca
Toshiaki Maki <tmaki@pivotal.io>
2020-05-26 20:17:19 +0900

    Add Google Translate bar

--- blog-frontend-ui/src/components/SearchBox.js
+++ blog-frontend-ui/src/components/SearchBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Redirect} from "react-router-dom";
 import {Input} from 'pivotal-ui/react/inputs';
+import {FormUnit} from 'pivotal-ui/react/forms';
 
 export class SearchBox extends React.Component {
     constructor(props) {
@@ -15,8 +16,11 @@ export class SearchBox extends React.Component {
     render() {
         return (
             <form method="get" className={"searchbox"} onSubmit={event => this.submit(event)}>
-                <label><Input icon="search" className="searchbox" name="query" type="search" placeholder="Search..."
-                              onChange={event => this.changeQ(event)}/></label>
+                <FormUnit>
+                    <label><Input icon="search" className="searchbox" name="query" type="search" placeholder="Search..."
+                                  onChange={event => this.changeQ(event)}/></label>
+                    <span id="google_translate_element"/>
+                </FormUnit>
                 <br/>
                 <br/>
                 {this.state.redirect &&
