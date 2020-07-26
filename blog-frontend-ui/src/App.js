806c5eb451d7e1003e374b3b5cc034fedb5d1732
Toshiaki Maki <tmaki@pivotal.io>
2020-07-26 21:24:33 +0900

    Use functional component

--- blog-frontend-ui/src/App.js
+++ blog-frontend-ui/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import {Tags} from "./tags/Tags";
 import {Categories} from "./categories/Categories";
@@ -26,63 +26,60 @@ import {SubscribeNote} from "./note/SubscribeNote";
 import {PasswordReset} from "./note/PasswordReset";
 import {Activation} from "./note/Activation";
 
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <h1>{`📝`} <Link to="/">BLOG.IK.AM</Link></h1>
-                    <Header/>
-                    <SearchBox/>
-                    <section id={"main"}>
-                        <article>
-                            <NavTab exact to="/">{`Home`}</NavTab>
-                            <NavTab exact to="/entries">{`Entries`}</NavTab>
-                            <NavTab to="/categories">{`Categories`}</NavTab>
-                            <NavTab to="/tags">{`Tags`}</NavTab>
-                            <NavTab exact to="/notes">{`Note`}</NavTab>
-                            <NavTab to="/aboutme" className={"nav-tab can-be-invisible"}>{`About`}</NavTab>
-                            <NavTab to="/info" className={"nav-tab can-be-invisible"}>{`Info`}</NavTab>
-                            <NavTab to="/dashboard" className={"nav-tab can-be-invisible"}>{`Dashboard`}</NavTab>
-                            <Switch>
-                                <Route exact path="/" render={() => <Home
-                                    renderedContent={this.props.renderedContent}/>}/>
-                                <Route path="/index.html" render={() => <Home
-                                    renderedContent={this.props.renderedContent}/>}/>
-                                <Route exact path="/entries" component={Entries}/>
-                                <Route path="/entries/:id" render={({match}) => <Entry
-                                    match={match}
-                                    renderedContent={this.props.renderedContent}/>}/>
-                                <Route exact path="/categories" component={Categories}/>
-                                <Route path="/categories/:id/entries" component={ByCategory}/>
-                                <Route exact path="/tags" component={Tags}/>
-                                <Route path="/tags/:id/entries" component={ByTag}/>
-                                <Route path="/aboutme" component={AboutMe}/>
-                                <Route path="/note/login" component={Login}/>
-                                <Route path="/note/signup" component={Signup}/>
-                                <Route path="/note/password_reset/:id" component={PasswordReset}/>
-                                <Route path="/note/readers/:readerId/activations/:activationLinkId"
-                                       component={Activation}/>
-                                <Route path="/notes/:id/subscribe" component={SubscribeNote}/>
-                                <Route path="/notes/:id" component={Note}/>
-                                <Route path="/notes" component={Notes}/>
-                                <Route exact path="/info" component={Info}/>
-                                <Route exact path="/dashboard" component={Dashboard}/>
-                                <Route path="/series/:id/entries" component={Series}/>
-                                <Route component={NoMatch}/>
-                            </Switch>
-                        </article>
-                    </section>
-                    <Links/>
-                    <Footer/>
-                </div>
-            </BrowserRouter>
-        );
-    }
+export default function App({renderedContent}) {
+    return (
+        <BrowserRouter>
+            <div>
+                <h1>{`📝`} <Link to="/">BLOG.IK.AM</Link></h1>
+                <Header/>
+                <SearchBox/>
+                <section id={"main"}>
+                    <article>
+                        <NavTab exact to="/">{`Home`}</NavTab>
+                        <NavTab exact to="/entries">{`Entries`}</NavTab>
+                        <NavTab to="/categories">{`Categories`}</NavTab>
+                        <NavTab to="/tags">{`Tags`}</NavTab>
+                        <NavTab exact to="/notes">{`Note`}</NavTab>
+                        <NavTab to="/aboutme"
+                                className={"nav-tab can-be-invisible"}>{`About`}</NavTab>
+                        <NavTab to="/info"
+                                className={"nav-tab can-be-invisible"}>{`Info`}</NavTab>
+                        <NavTab to="/dashboard"
+                                className={"nav-tab can-be-invisible"}>{`Dashboard`}</NavTab>
+                        <Switch>
+                            <Route exact path="/" render={() => <Home
+                                renderedContent={renderedContent}/>}/>
+                            <Route path="/index.html" render={() => <Home
+                                renderedContent={renderedContent}/>}/>
+                            <Route exact path="/entries" component={Entries}/>
+                            <Route path="/entries/:id"
+                                   render={({match}) => <Entry match={match}
+                                                               renderedContent={renderedContent}/>}/>
+                            <Route exact path="/categories" component={Categories}/>
+                            <Route path="/categories/:id/entries" component={ByCategory}/>
+                            <Route exact path="/tags" component={Tags}/>
+                            <Route path="/tags/:id/entries" component={ByTag}/>
+                            <Route path="/aboutme" component={AboutMe}/>
+                            <Route path="/note/login" component={Login}/>
+                            <Route path="/note/signup" component={Signup}/>
+                            <Route path="/note/password_reset/:id"
+                                   component={PasswordReset}/>
+                            <Route
+                                path="/note/readers/:readerId/activations/:activationLinkId"
+                                component={Activation}/>
+                            <Route path="/notes/:id/subscribe" component={SubscribeNote}/>
+                            <Route path="/notes/:id" component={Note}/>
+                            <Route path="/notes" component={Notes}/>
+                            <Route exact path="/info" component={Info}/>
+                            <Route exact path="/dashboard" component={Dashboard}/>
+                            <Route path="/series/:id/entries" component={Series}/>
+                            <Route component={NoMatch}/>
+                        </Switch>
+                    </article>
+                </section>
+                <Links/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    );
 }
-
-export default App;
