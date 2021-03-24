206ea82ce22ab6e8ec36c6d23825ebd24653e33b
Toshiaki Maki <tmaki@pivotal.io>
2021-03-24 23:31:16 +0900

    Assign object to a variable before exporting as module

--- blog-frontend-ui/src/urlProvider.js
+++ blog-frontend-ui/src/urlProvider.js
@@ -1,4 +1,5 @@
-export default {
+const urlMap = {
     BLOG_API: process.env.REACT_APP_BLOG_API.toLowerCase() === "auto" ? `${document.location.protocol}//${document.location.hostname.replace(".", "-api.")}` : process.env.REACT_APP_BLOG_API,
     BLOG_UI: process.env.REACT_APP_BLOG_UI.toLowerCase() === "auto" ? `${document.location.protocol}//${document.location.hostname}` : process.env.REACT_APP_BLOG_UI
-}
+};
+export default urlMap;
