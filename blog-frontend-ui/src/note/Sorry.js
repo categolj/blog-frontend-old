d0e13cb926beea9f4bbf3d1b14a7e1e807961af7
Toshiaki Maki <tmaki@vmware.com>
2021-03-26 17:19:36 +0900

    Remove sorry message

--- blog-frontend-ui/src/note/Sorry.js
+++ blog-frontend-ui/src/note/Sorry.js
@@ -1,18 +1,10 @@
 import React from "react";
-import {WarningAlert} from "pivotal-ui/react/alerts";
 
 import 'pivotal-ui/css/ellipsis';
 
 export class Sorry extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <br/>
-                <WarningAlert>2021-03-26: 内部システム移行に伴う問題で、記事の取得ができない状態です。(CORSの問題です。)<br/>
-                    2021-03-29までには修正の予定ですが、急ぎで記事の内容が必要な場合は<a
-                        href={'https://twitter.com/making'}>@making</a>までご連絡ください。
-                </WarningAlert>
-                <br/>
-            </React.Fragment>);
+            <React.Fragment></React.Fragment>);
     }
 }
