df267abb8d577b48572d7b310219b35af993fd49
Toshiaki Maki <tmaki@vmware.com>
2021-04-07 18:59:37 +0900

    Support building native-image

--- blog-frontend-server/src/main/java/am/ik/blog/App.java
+++ blog-frontend-server/src/main/java/am/ik/blog/App.java
@@ -2,22 +2,60 @@ package am.ik.blog;
 
 import am.ik.blog.dashboard.DashboardHandler;
 import am.ik.blog.entries.BlogHandler;
+import am.ik.blog.entries.Prerender;
+import am.ik.blog.entries.PrerenderClient;
+import am.ik.blog.prometheus.Prometheus;
+import com.github.benmanes.caffeine.cache.AsyncCacheLoader;
 import io.micrometer.core.instrument.config.MeterFilter;
 import reactor.netty.http.client.HttpClient;
 
+import org.springframework.aop.SpringProxy;
+import org.springframework.aop.framework.Advised;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.DecoratingProxy;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ProxyHint;
+import org.springframework.nativex.hint.ResourceHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties({ Prometheus.class, Prerender.class })
+@NativeHint(
+		options = { "--enable-http" },
+		types = {
+				@TypeHint(types = { AsyncCacheLoader.class }, typeNames = {
+						"com.github.benmanes.caffeine.cache.SSLMSA",
+						"com.github.benmanes.caffeine.cache.PSAMW",
+						"am.ik.blog.entries.PrerenderClientImpl$PrerenderAsyncCacheLoader"
+				}),
+				@TypeHint(typeNames = {
+						"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinActiveMqSenderConfiguration",
+						"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinRabbitSenderConfiguration",
+						"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinKafkaSenderConfiguration",
+						"org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinRestTemplateSenderConfiguration"
+				})
+		},
+		proxies = {
+				@ProxyHint(types = { PrerenderClient.class, SpringProxy.class, Advised.class, DecoratingProxy.class })
+		},
+		resources = {
+				@ResourceHint(patterns = {
+						"META-INF/resources/.*",
+						"META-INF/resources/static/css/.*",
+						"META-INF/resources/static/js/.*",
+						"META-INF/resources/static/media/.*"
+				})
+		}
+)
 public class App {
 
 	public static void main(String[] args) {
@@ -35,7 +73,7 @@ public class App {
 	}
 
 	@Bean
-	public MeterRegistryCustomizer meterRegistryCustomizer() {
+	public MeterRegistryCustomizer<?> meterRegistryCustomizer() {
 		return registry -> registry.config() //
 				.meterFilter(MeterFilter.deny(id -> {
 					String uri = id.getTag("uri");
--- blog-frontend-server/src/main/java/am/ik/blog/entries/PrerenderClient.java
+++ blog-frontend-server/src/main/java/am/ik/blog/entries/PrerenderClient.java
@@ -1,53 +1,9 @@
 package am.ik.blog.entries;
 
-import java.net.URI;
-import java.time.Duration;
-
-import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
-import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
-public class PrerenderClient {
-
-	private static final Logger log = LoggerFactory.getLogger(PrerenderClient.class);
-
-	private final WebClient webClient;
-
-	private final Prerender prerender;
-
-	private final AsyncLoadingCache<String, String> cache = Caffeine.newBuilder()
-			.maximumSize(100)
-			.expireAfterAccess(Duration.ofHours(1))
-			.removalListener((key, value, cause) -> log.info("Removing cache({}) because of {}", key, cause))
-			.buildAsync((key, __) -> PrerenderClient.this.prerender(HttpMethod.GET, key).toFuture());
-
-	public PrerenderClient(WebClient.Builder builder, Prerender prerender) {
-		this.webClient = builder.build();
-		this.prerender = prerender;
-	}
-
-	@NewSpan("prerender")
-	public Mono<String> invoke(HttpMethod method, String url) {
-		if (method == HttpMethod.HEAD) {
-			return this.prerender(method, url);
-		}
-		return Mono.fromFuture(this.cache.get(url));
-	}
-
-	private Mono<String> prerender(HttpMethod method, String url) {
-		final URI uri = URI.create(prerender.getUrl() + "/" + url);
-		return this.webClient.method(method)
-				.uri(uri)
-				.header("X-Prerender-Token", this.prerender.getToken())
-				.retrieve()
-				.bodyToMono(String.class);
-	}
+public interface PrerenderClient {
+	Mono<String> invoke(HttpMethod method, String url);
 }
