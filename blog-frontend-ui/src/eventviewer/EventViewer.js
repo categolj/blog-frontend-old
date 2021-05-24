b7732417ca52181378cf8b5ec660cfcd5e42b99c
Toshiaki Maki <tmaki@vmware.com>
2021-05-25 01:34:49 +0900

    Use location.origin

--- blog-frontend-ui/src/eventviewer/EventViewer.js
+++ blog-frontend-ui/src/eventviewer/EventViewer.js
@@ -69,7 +69,7 @@ export class EventViewer extends React.Component {
                         in <code>application.properties</code> as follows
                         <pre><code>
                         management.endpoints.web.exposure.include=startup<br/>
-                        management.endpoints.web.cors.allowed-origins=https://blog.ik.am<br/>
+                        management.endpoints.web.cors.allowed-origins={location.origin}<br/>
                         management.endpoints.web.cors.allowed-methods=POST
                         </code></pre>
                         <br/>
