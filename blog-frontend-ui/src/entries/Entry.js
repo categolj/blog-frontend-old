07d06b4fcb77f70d15342c6a8d90300c436f0585
Toshiaki Maki <tmaki@vmware.com>
2021-08-10 17:54:29 +0900

    Fix yKey

--- blog-frontend-ui/src/entries/Entry.js
+++ blog-frontend-ui/src/entries/Entry.js
@@ -175,7 +175,7 @@ export class Entry extends React.Component {
                        width={155}
                        height={30}
                        xKey='timestamp'
-                       yKey='count'/>
+                       yKey='browser'/>
             <Divider/>
             <p ref={this.ref} dangerouslySetInnerHTML={Entry.content(entry)}>
             </p>
