f519185635ff719aac8f2d4b0a5116a3aadb8398
Toshiaki Maki <tmaki@vmware.com>
2021-09-13 11:28:56 +0900

    Fix urlProvider

--- blog-frontend-ui/src/urlProvider.js
+++ blog-frontend-ui/src/urlProvider.js
@@ -1,5 +1,5 @@
 const urlMap = {
-    BLOG_API: process.env.REACT_APP_BLOG_API.toLowerCase() === "auto" ? `${document.location.protocol}//${document.location.hostname.replace(".", "-api.")}` : process.env.REACT_APP_BLOG_API,
+    BLOG_API: process.env.REACT_APP_BLOG_API.toLowerCase() === "auto" ? `${document.location.protocol}//${document.location.hostname.replace("-frontend.", "-api.")}` : process.env.REACT_APP_BLOG_API,
     BLOG_UI: process.env.REACT_APP_BLOG_UI.toLowerCase() === "auto" ? `${document.location.protocol}//${document.location.hostname}` : process.env.REACT_APP_BLOG_UI
 };
 export default urlMap;
