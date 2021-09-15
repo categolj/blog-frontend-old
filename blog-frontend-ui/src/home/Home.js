001c9a39d255a2be5b9a38cf5475a61e2e86bf5a
Toshiaki Maki <tmaki@vmware.com>
2021-09-15 12:29:08 +0900

    Add access count report

--- blog-frontend-ui/src/home/Home.js
+++ blog-frontend-ui/src/home/Home.js
@@ -8,18 +8,56 @@ import readCountService from "../entries/ReadCountService";
 import Sparkline from "../components/Sparkline";
 import {Helmet} from "react-helmet-async";
 
+function sumSince(result, date) {
+    return result.filter((x) => x.browser > 0 &&
+        new Date(x.timestamp).getTime() >= date.getTime())
+        .map(x => x.browser)
+        .reduce((x, y) => x + y);
+}
+
+function resetHMS(date) {
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    return date;
+}
+
 export function Home({renderedContent}) {
-    const [readCounts, setReadCounts] = useState([]);
+    const [readCounts, setReadCounts] = useState({
+        sumOfToday: 0,
+        sumOfYesterday: 0,
+        sumOfDayBeforeYesterday: 0,
+        sumOfThisWeek: 0,
+        results: []
+    });
     useEffect(() => {
         readCountService.readCountAll()
-            .then(result =>
-                setReadCounts(result.map(x => {
-                    return {
-                        timestamp: new Date(x.timestamp).toLocaleString(),
-                        browser: (x.browser || 0),
-                        total: (x.browser || 0) + (x.nonBrowser || 0)
-                    }
-                })));
+            .then(result => {
+                const today = resetHMS(new Date());
+                const yesterday = resetHMS(new Date());
+                const dayBeforeYesterday = resetHMS(new Date());
+                const startOfThisWeek = resetHMS(new Date());
+                yesterday.setDate(today.getDate() - 1);
+                dayBeforeYesterday.setDate(yesterday.getDate() - 1);
+                startOfThisWeek.setDate(today.getDate() - today.getDay());
+                const sumOfToday = sumSince(result, today);
+                const sumOfYesterday = sumSince(result, yesterday) - sumOfToday;
+                const sumOfDayBeforeYesterday = sumSince(result, dayBeforeYesterday) - sumOfToday - sumOfYesterday;
+                const sumOfThisWeek = sumSince(result, startOfThisWeek);
+                setReadCounts({
+                    results: result.map(x => {
+                        return {
+                            timestamp: new Date(x.timestamp).toLocaleString(),
+                            browser: (x.browser || 0),
+                            total: (x.browser || 0) + (x.nonBrowser || 0)
+                        }
+                    }),
+                    sumOfToday: sumOfToday,
+                    sumOfYesterday: sumOfYesterday,
+                    sumOfDayBeforeYesterday: sumOfDayBeforeYesterday,
+                    sumOfThisWeek: sumOfThisWeek,
+                });
+            });
     }, []);
     return (<Panel>
         <Helmet prioritizeSeoTags>
@@ -37,10 +75,20 @@ export function Home({renderedContent}) {
         <GitHub/>
         <Presentations/>
         <h3 id="access-rate">Access Rate</h3>
-        <Sparkline data={readCounts}
-                   width={320}
-                   height={60}
+        <Sparkline data={readCounts.results}
+                   width={640}
+                   height={100}
                    xKey='timestamp'
                    yKey='browser'/>
+        <dl>
+            <dt>Today</dt>
+            <dd>{readCounts.sumOfToday}</dd>
+            <dt>Yesterday</dt>
+            <dd>{readCounts.sumOfYesterday}</dd>
+            <dt>Day before yesterday</dt>
+            <dd>{readCounts.sumOfDayBeforeYesterday}</dd>
+            <dt>This week</dt>
+            <dd>{readCounts.sumOfThisWeek}</dd>
+        </dl>
     </Panel>);
 }
